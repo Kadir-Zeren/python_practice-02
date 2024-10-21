3a1c0d2d6e433140d10ce9d489e3c4d626984500
kadir-zeren <k4dzeren@gmail.com>
2024-10-21 15:34:22 +0300

    python example

--- python_02.py
+++ python_02.py
@@ -2,4 +2,148 @@
 # bu ikinci comment
 # bu ucuncu yorum
 
+
+a = "bu benim yazdigim string \
+stringe devam ediyorum"
+
+print(a)
+
+a = "bu benim yazdigim string \nstringe devam ediyorum"
+
+print(a)
+
+Con_PEP_N_R1 = "Conventional (PEP 8) Naming Rules"
+
+planet = 'jupyter'
+price = 140
+pi_number = 3.14
+
 school = "clarusway"
+print(school)
+
+sayi = 35
+print(sayi)
+
+ğğğ = 5
+print(ğğğ) # turkce karakter hata vermez, ama kullanmayin
+
+x = 3
+y = 7
+z = 21
+print(x+y+z)
+
+school = "clarusway"
+sayi = 35
+sayi2 = 35.0#değişken atamayı öğrendik
+print( school, sayi, sayi2)
+
+my_age = 33
+your_age = 30
+my_age = your_age
+print(my_age)
+
+variable = 3.14
+var_one = 'something'
+
+okul = 'Clarusway'
+
+l = 3.14 # This is lowercase letter el
+I = 3.14 # This is uppercase letter eye
+# 1 = 'something wrong' # This is number one.
+
+time_O = '3.14'# This is uppercase letter "O"
+time_0 = '3.14'# This is number zero
+
+help('keywords' )
+
+# dil = python # python' is not defined
+
+# 5 = sayi # SyntaxError: cannot assign to literal
+
+# 'clarusway' = okul # SyntaxError: cannot assign to literal
+
+variable = 'degisken'
+isim_soyisim = 'ali guzel'
+
+planet = 'jupyter'
+price = 140
+
+degisken = 'bu bir string'
+print(degisken)
+
+degisken = 5
+print(degisken)
+
+x = y = z = "same"
+print(x)
+print(y)
+print(z)
+
+a = 'degisken_1'
+b = 5
+c = 7.6
+print(a,b,c)
+
+january = march = may = july = august = october = december = 31
+# multi assignments in a single line
+april = june = september = november = 30
+february = 28
+print(january, february, march, april, may, june, july, august, september, october,
+november, december)
+
+ali = None
+print(ali)
+
+website = "apple.com"
+print(website)
+# assigning a new variable to website
+website = "clarusway.com"
+print(website)
+
+first_number = 100
+second_number = first_number
+print(second_number)
+
+x = 15
+y = 33
+z = x
+x = y
+print(x)
+print(y)
+print(z)
+
+a, b, c = 5, 3.2, "Hello"
+print(a)
+print(b)
+print(c)
+
+man = "andrew"
+color = "green"
+age = 32
+pi = 3.14
+color = "yellow"
+age = 44
+man = "joseph"
+print (man, age, color)
+
+my_text = 'being a good person'
+print(my_text)
+
+str_number = '1923'
+str_sign = '%(#& *?- '
+print(str_number)
+print(str_sign)
+print(type(str_number), type(str_sign))
+
+my_integer = 40
+negative_num = - 18
+print(my_integer)
+print(negative_num)
+
+x = 39
+v = "11"
+y = "2.5"
+z = "I am at_"
+print(x-int(v))
+print(x-float(y))
+print(z+str(x))
